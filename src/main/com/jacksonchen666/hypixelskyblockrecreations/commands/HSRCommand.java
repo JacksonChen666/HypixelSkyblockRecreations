1dc247b0a36222196cfac23e9026dcbdaf799b5e
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2020-11-03 07:53:28 +0100

    more messages, added items map, and support for getting multiple items

--- src/main/com/jacksonchen666/hypixelskyblockrecreations/commands/HSRCommand.java
+++ src/main/com/jacksonchen666/hypixelskyblockrecreations/commands/HSRCommand.java
@@ -7,17 +7,20 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
+import java.util.*;
 
-public class HSRCommand implements CommandExecutor {
+public class HSRCommand implements CommandExecutor, TabCompleter {
     private final JavaPlugin plugin;
+    @SuppressWarnings("SpellCheckingInspection")
     public static final String commandName = "hypixelskyblockrecreations";
+    private static final Map<String, ItemStack> items = new HashMap<>();
 
     public HSRCommand(JavaPlugin plugin) {
         this.plugin = plugin;
@@ -29,19 +32,51 @@ public class HSRCommand implements CommandExecutor {
         return plugin.getConfig().getString(path);
     }
 
+    private String getText(String path, String prefix) {
+        return getText(path).replace("{prefix}", prefix);
+    }
+
+    public static void putItems(String key, ItemStack itemStack) {
+        items.put(key, itemStack);
+    }
+
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         if (!(commandSender instanceof Player)) { // Console. Do your thing or just ignore.
             plugin.getServer().getConsoleSender().sendMessage(ChatColors.color(getText("errors.console_execution").replace("{prefix}", getText("errors.error_prefix"))));
             return false;
         }
 
         Player p = (Player) commandSender;
-        ItemStack book = new ItemStack(Material.STONE_HOE);
-        ItemMeta meta = book.getItemMeta();
-        Objects.requireNonNull(meta).addEnchant(HypixelSkyblockRecreations.getCustomEnchantments().REPLANTING, 1, false);
-        book.setItemMeta(meta);
-        p.getInventory().addItem(book);
-        return true;
+        String prefix = getText("prefix");
+        if (args.length >= 2) {
+            if (args[0].equals("giveItem")) {
+                for (String key : items.keySet()) {
+                    if (args[1].equals(key)) {
+                        p.getInventory().addItem(items.get(key));
+                        return true;
+                    }
+                }
+                p.sendMessage(ChatColors.color(getText("errors.unknown_item", prefix).replace("{name}", args[1])));
+                return false;
+            }
+        }
+        else {
+            p.sendMessage(ChatColors.color(getText("errors.missing_argument", prefix).replace("{arguments}", "action")));
+        }
+        return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 0) {
+            return Collections.singletonList("giveItem");
+        }
+        else if (args.length == 1) {
+            return new ArrayList<>(items.keySet());
+        }
+        else {
+            return Collections.emptyList();
+        }
     }
 }
--- src/main/com/jacksonchen666/hypixelskyblockrecreations/enchantments/replanting/Replanting.java
+++ src/main/com/jacksonchen666/hypixelskyblockrecreations/enchantments/replanting/Replanting.java
@@ -1,15 +1,27 @@
 package com.jacksonchen666.hypixelskyblockrecreations.enchantments.replanting;
 
+import com.jacksonchen666.hypixelskyblockrecreations.HypixelSkyblockRecreations;
+import com.jacksonchen666.hypixelskyblockrecreations.commands.HSRCommand;
 import com.jacksonchen666.hypixelskyblockrecreations.enchantments.base.BaseEnchantments;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public class Replanting extends BaseEnchantments {
     private static final String name = "Replanting";
 
     public Replanting(JavaPlugin plugin) {
         super(new NamespacedKey(plugin, name));
+        ItemStack item = new ItemStack(Material.STONE_HOE);
+        ItemMeta meta = item.getItemMeta();
+        Objects.requireNonNull(meta).addEnchant(HypixelSkyblockRecreations.getCustomEnchantments().REPLANTING, 1, false);
+        item.setItemMeta(meta);
+        HSRCommand.putItems(name.toLowerCase(), item);
     }
 
     @Override
