1f0ef68db3c131a9faedb0787ce28a614e872395
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2020-11-09 12:25:26 +0100

    fixed testing

--- src/test/com/jacksonchen666/hypixelskyblockrecreations/enchantments/replanting/ReplantTest.java
+++ src/test/com/jacksonchen666/hypixelskyblockrecreations/enchantments/replanting/ReplantTest.java
@@ -7,38 +7,31 @@ import com.jacksonchen666.hypixelskyblockrecreations.HypixelSkyblockRecreations;
 import com.jacksonchen666.hypixelskyblockrecreations.commands.HSRCommand;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.inventory.ItemStack;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-
 public class ReplantTest {
-    private ServerMock server;
     private PlayerMock player1;
 
     @Before
     public void setUp() {
-        class ServerMock2 extends ServerMock {
-
-        }
-        server = MockBukkit.mock(new ServerMock2());
+        ServerMock server = MockBukkit.mock();
         server.addSimpleWorld("world");
         player1 = server.addPlayer();
         MockBukkit.load(HypixelSkyblockRecreations.class);
         player1.setItemInHand(HSRCommand.getItems().get("replanting").createItem());
+        player1.getInventory().addItem(new ItemStack(Material.WHEAT_SEEDS));
     }
 
     @Test
     public void testReplanting() {
-        System.out.println(player1.getInventory().getItemInMainHand());
-        System.out.println(Arrays.toString(player1.getInventory().getContents()));
         player1.getWorld().getBlockAt(0, 3, 0).setType(Material.FARMLAND);
         Block block = player1.getWorld().getBlockAt(0, 4, 0);
         block.setType(Material.WHEAT);
         player1.simulateBlockBreak(block);
-        server.getScheduler().performOneTick();
         Assert.assertEquals(Material.WHEAT, block.getType());
     }
 
